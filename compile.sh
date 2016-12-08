0bd19a5847326937340b4a3981a002ec4c2e2b95
Jose Eduardo <jose.eduardo.gd@gmail.com>
2016-12-08 20:51:14 +0000

    Add script to compile vim from source

---
+++ compile.sh
@@ -0,0 +1,26 @@
+set -e
+
+# sudo apt-get install libxpm-dev
+
+./configure --with-features=huge \
+    --enable-fail-if-missing \
+    --with-x \
+    --enable-multibyte \
+    --enable-rubyinterp=yes \
+    --enable-pythoninterp=yes \
+    --with-python-config-dir=/usr/lib/python2.7/config \
+    --disable-gui \
+    --enable-luainterp=yes \
+    # --enable-gtk2-check \
+    # --enable-gnome-check \
+    # --enable-gui=gnome2 \
+    # --enable-python3interp=yes \
+    # --with-python3-config-dir=/usr/lib/python3.5/config \
+    # --enable-perlinterp=yes \
+    # --enable-gui=gtk2 \
+    # --enable-cscope \
+    # --prefix=/usr
+# make VIMRUNTIMEDIR=/usr/share/vim/vim74/
+make VIMRUNTIMEDIR=~/workspace/vim/runtime/
+
+set +e
