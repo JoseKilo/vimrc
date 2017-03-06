58a21a93cdc5a8b097922045d0670f5ef8c6890a
Jose Garcia <joseeduardo.garcia@rockabox.com>
2017-03-06 17:42:46 +0000

    No need to pass config dir

--- compile.sh
+++ compile.sh
@@ -7,14 +7,15 @@ set -e
     --with-x \
     --enable-multibyte \
     --enable-rubyinterp=yes \
-    --enable-pythoninterp=yes \
-    --with-python-config-dir=/usr/lib/python2.7/config \
     --disable-gui \
     --enable-luainterp=yes \
+    --enable-pythoninterp=yes \
+    --enable-python3interp=yes \
     # --enable-gtk2-check \
     # --enable-gnome-check \
     # --enable-gui=gnome2 \
     # --enable-python3interp=yes \
+    # --with-python-config-dir=/usr/lib/python2.7/config \
     # --with-python3-config-dir=/usr/lib/python3.5/config \
     # --enable-perlinterp=yes \
     # --enable-gui=gtk2 \
