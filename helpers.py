4796bdc9a01365132c940e6595f5c63f4059ec17
Jose Garcia <joseeduardo.garcia@rockabox.com>
2017-04-25 17:03:15 +0100

    Python __doc__ test helper

---
+++ helpers.py
@@ -0,0 +1,28 @@
+import re
+import sys
+from difflib import ndiff
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+
+def test_module_doc(doc, main):
+    search = re.search(r'^\n(.*)\n\n(.*)$', doc, re.DOTALL)
+    test_input = search.group(1)
+    expected_test_output = search.group(2)
+
+    sys.stdin = StringIO(test_input)
+    sys.stdout = StringIO()
+
+    main()
+
+    sys.stdin = sys.__stdin__
+    test_result = str(sys.stdout.getvalue())
+    sys.stdout = sys.__stdout__
+
+    diff = ndiff(expected_test_output.splitlines(1), test_result.splitlines(1))
+    error_message = '\n{}\n{}\n'.format(test_result, expected_test_output)
+    error_message = '\n{}'.format(''.join(diff))
+
+    assert test_result == expected_test_output, error_message
